b016d8a903ab9656e779111dea30feedcf05b302
Diogo Nunes <up201808546@fe.up.pt>
2021-11-18 12:43:34 +0000

    fibRec, fibLista e fibListaInfinita done

--- TP1/Fib.hs
+++ TP1/Fib.hs
@@ -1,9 +1,19 @@
-
---fibRec :: (Integral a) => a -> a
+-- fibRec
+fibRec :: (Integral a) => a -> a
+fibRec 0 = 0
+fibRec 1 = 1
+fibRec n = fibRec(n-2) + fibRec(n-1)
+    -- n >= 0 = fibRec(n-2) + fibRec(n-1)
+    -- | otherwise = error "Error: Negative input"
 
 --fibLista
+fibLista :: Int -> Int
+fibLista n = map fibRec [0..n] !! n
 
 --fibListaInfinita
+fibListaInfinita :: Num a => Int -> a
+fibListaInfinita n = fibLI !! n
+    where fibLI = 0 : 1 : zipWith (+) fibLI (tail fibLI)
 
 -- fibRecBN
 
